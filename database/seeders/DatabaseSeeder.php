56d3720cca225074ffec22e7f0d3c3480594d79c
dina <dinaelhadidy966@gmail.com>
2025-03-09 17:55:56 +0200

    Create posts seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10)->create()->each(function ($user) {
+            Post::factory()
+                ->count(50) 
+                ->for($user) 
+                ->create();
+        });
     }
 }
