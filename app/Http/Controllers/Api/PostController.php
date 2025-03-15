b60dfeda42ed0d650faf5e07cfceadc79042e239
dina <dinaelhadidy966@gmail.com>
2025-03-15 15:31:32 +0200

    Add show and store methods to PostController for retrieving and creating posts

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -13,4 +13,25 @@ class PostController extends Controller
         $posts = Post::paginate(10); 
         return PostResource::collection($posts);
     }
+
+    public function show($id) {
+        $post = Post::findOrFail($id);
+        return new PostResource($post);
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'posted_by' => 'required',
+        ]);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'user_id' => $request->posted_by,
+        ]);
+
+        return new PostResource($post);
+    }
 }
